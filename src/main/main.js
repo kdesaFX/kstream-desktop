c50f75dd866cdb6b1a01ec46035364fb2611662b
Kdesa <kdesa@users.noreply.github.com>
2026-09-19 19:09:06 -0500

    fix: balance desktop header edge spacing

--- src/main/main.js
+++ src/main/main.js
@@ -106,7 +106,7 @@ const DESKTOP_CHROME_CSS = `
   #kstream-window-controls {
     position: fixed;
     top: 0;
-    right: 0;
+    right: 36px;
     z-index: 2147483647;
     display: flex;
     height: 32px;
