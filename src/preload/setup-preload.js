671dc2a8e71e2ba01c39fac8f0eed9a47be60dd2
Kdesa <kdesabiz@gmail.com>
2026-09-21 18:39:54 -0500

    Initialize public desktop release channel

--- src/preload/setup-preload.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('kstreamSetup', {
-  getInfo: () => ipcRenderer.invoke('setup:getInfo'),
-  install: () => ipcRenderer.invoke('setup:install'),
-  portable: () => ipcRenderer.invoke('setup:portable'),
-});
